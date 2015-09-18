e6879ea1410b49d4245d4df2018778ed6c543f5f
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-18 11:30:02 +0200

    change tidy5 executable to tidy

--- reformat-html.sh
+++ reformat-html.sh
@@ -3,7 +3,7 @@
 if [ "${1}" = "--ci-mode" ]; then
     shift
     mkdir -p build/log
-    tidy5 -config tidy5.conf -modify -file build/log/tidy5-reformat.log web/index.html
+    tidy -config tidy.conf -modify -file build/log/tidy-reformat.log web/index.html
 else
-    tidy5 -config tidy5.conf -modify web/index.html
+    tidy -config tidy.conf -modify web/index.html
 fi
--- run-lint-check.sh
+++ run-lint-check.sh
@@ -3,7 +3,7 @@
 if [ "${1}" = "--ci-mode" ]; then
     shift
     mkdir -p build/log
-    tidy5 -config tidy5.conf -file build/log/tidy5-lint.log web/index.html
+    tidy -config tidy.conf -file build/log/tidy-lint.log web/index.html
 else
-    tidy5 -config tidy5.conf web/index.html
+    tidy -config tidy.conf web/index.html
 fi
