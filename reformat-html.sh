10540b9207a8e6d35f8dd524dcf4af71e1dabd30
Alexander Reitzel <funtimecoding@gmail.com>
2015-09-18 11:10:56 +0200

    first commit

---
+++ reformat-html.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -e
+
+if [ "${1}" = "--ci-mode" ]; then
+    shift
+    mkdir -p build/log
+    tidy5 -config tidy5.conf -modify -file build/log/tidy5-reformat.log web/index.html
+else
+    tidy5 -config tidy5.conf -modify web/index.html
+fi
